b688226b072b9dc61f2c67b1b6e51151b347909c
brinkz <isabel.sbg11@gmail.com>
2022-08-15 17:59:04 -0600

    otra forma de calcular usando una funcion

--- fizzbuzzPlatzi.js
+++ fizzbuzzPlatzi.js
@@ -1,18 +1,23 @@
 var fizz = "Fizz";
 var buzz = "Buzz";
-var divisible = false;
 
 for(var i = 1; i <= 100; i++){
-    divisible = false;
-    if(i%3 == 0){
+
+    if(divisible(i, 3)){
         document.write(fizz);
-        divisible = true;
     }
-    if(i%5 == 0){
+    if(divisible(i, 5)){
         document.write(buzz);
-        divisible = true;
-    }else if(!divisible){
+    }else if(!divisible(i,3) && !divisible(i, 5)){
         document.write(i);
     }
     document.write("<br />");
+}
+
+function divisible(num, divisor){
+    if(num % divisor == 0){
+        return true;
+    }else{
+        return false;
+    }
 }
